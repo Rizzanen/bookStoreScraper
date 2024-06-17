b563c8e553c925b8db85178a72f173eb744dc7b7
Rizzanen <aev.rissanen@gmail.com>
2024-06-17 19:12:28 +0300

    Added functionality where the url of the request is changed after a page is scraped allowing the app to scrape each product page. the books data is stored in a list as key value pairs for future analysis with pandas.

--- bookClass.py
+++ bookClass.py
@@ -2,13 +2,11 @@ class Book:
 
     def __init__(self, title, author, price, rating):
         self.title = title
-        self.author = author
         self.price = price
         self.rating = rating
 
     def display_info(self):
         print(f"Title: {self.title}")
-        print(f"Author: {self.author}")
         print(f"Price: {self.price}")
         print(f"Rating: {self.rating}")
         
--- scraper.py
+++ scraper.py
@@ -3,20 +3,28 @@ from bs4 import BeautifulSoup
 import bookClass as Book
 import functions
 
-
 def scraper():
-    pageToScrape = requests.get("https://books.toscrape.com/catalogue/category/books/horror_31/index.html")
-    soup = BeautifulSoup(pageToScrape.text, "html.parser")
-    listings = soup.findAll("li", attrs={"class", "col-xs-6 col-sm-4 col-md-3 col-lg-3"})
+    responseStatus = 200
+    webpageNumber = 1
+    books = []
+    print("scraping book store...")
+    while responseStatus == 200:
+        try:
+            pageToScrape = requests.get(f"https://books.toscrape.com/catalogue/page-{webpageNumber}.html")
+            responseStatus = pageToScrape.status_code
+            
+            soup = BeautifulSoup(pageToScrape.text, "html.parser")
+            listings = soup.findAll("li", attrs={"class", "col-xs-6 col-sm-4 col-md-3 col-lg-3"})
     
-
-    #     listings = container.findAll("listing-search")
-    #     print(listings)
-    for listing in listings:
-        price = listing.find("p", class_="price_color")
-        name = functions.getName(listing)
-        starRating = functions.getRating(listing)
-        
-        print(f"Name: {name} \nPrice: {price.text[2:]}€ \nStar rating: {starRating}\n")
-        
+            for listing in listings:
+                price = listing.find("p", class_="price_color")
+                name = functions.getName(listing)
+                starRating = functions.getRating(listing)
+                bookDictionary= {"Title" : name, "Price" : price.text[2:], "rating": starRating}
+                books.append(bookDictionary)
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+            return
+        webpageNumber = webpageNumber +1
+    print(books)
 scraper()
